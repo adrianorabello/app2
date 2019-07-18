21ae37f9ae5776ddfb37aa7b55cc6be7c758be42
adrianorabello <rabellocbmes@gmail.com>
2019-07-18 20:46:42 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ofertas.service.ts
@@ -0,0 +1,92 @@
+
+
+
+
+
+import { Oferta } from './shared/oferta.model';
+
+
+export class OfertasService{
+
+    public ofertas:Oferta[] =[this.criarOferta()] /*  [
+        [
+            let oferta1 = new Oferta(),
+            {
+                id: 1,
+                categoria: "restaurante",
+                titulo: "Super Burger",
+                descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
+                anunciante: "Original Burger",
+                valor: 29.90,
+                destaque: true,
+                imagens: [
+                    {url: "/assets/ofertas/1/img1.jpg"},
+                    {url: "/assets/ofertas/1/img2.jpg"},
+                    {url: "/assets/ofertas/1/img3.jpg"},
+                    {url: "/assets/ofertas/1/img4.jpg"}
+                ]
+            },
+            {
+                id: 2,
+                categoria: "restaurante",
+                titulo: "Cozinha Mexicana",
+                descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
+                anunciante: "Mexicana",
+                valor: 32.90,
+                destaque: true,
+                imagens: [
+                    {url: "/assets/ofertas/2/img1.jpg"},
+                    {url: "/assets/ofertas/2/img2.jpg"},
+                    {url: "/assets/ofertas/2/img3.jpg"},
+                    {url: "/assets/ofertas/2/img4.jpg"}
+                ]
+            
+            },
+            {
+                id: 4,
+                categoria: "diversao",
+                titulo: "Estância das águas",
+                descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
+                anunciante: "Estância das águas",
+                valor: 31.90,
+                destaque: true,
+                imagens: [
+                    {url: "/assets/ofertas/3/img1.jpg"},
+                    {url: "/assets/ofertas/3/img2.jpg"},
+                    {url: "/assets/ofertas/3/img3.jpg"},
+                    {url: "/assets/ofertas/3/img4.jpg"},
+                    {url: "/assets/ofertas/3/img5.jpg"},
+                    {url: "/assets/ofertas/3/img6.jpg"}
+                ]
+            }
+        ]
+    ] */
+
+    public getOfertas(): Array<Oferta>{
+
+        return this.ofertas;
+    }
+
+
+    criarOferta():Oferta
+    {
+       let flag = new Oferta();
+
+       flag.id = 4,
+       flag.categoria = "diversao",
+       flag.titulo = "Estância das águas"
+       flag.descricao_oferta = "Diversão garantida com piscinas, trilhas e muito mais."
+       flag.anunciante = "Estância das águas"
+       flag.valor = 31.90
+       flag.destaque = true
+       flag.imagens = [
+           {url: "/assets/ofertas/3/img1.jpg"},
+           {url: "/assets/ofertas/3/img2.jpg"},
+           {url: "/assets/ofertas/3/img3.jpg"},
+           {url: "/assets/ofertas/3/img4.jpg"},
+           {url: "/assets/ofertas/3/img5.jpg"},
+           {url: "/assets/ofertas/3/img6.jpg"}]
+
+           return flag;
+    }
+}
